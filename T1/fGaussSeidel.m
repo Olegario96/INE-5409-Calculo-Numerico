35a1f1b58282b8b78b8c748d1e84ae48d10c8342
Olegario96 <gustavo-olegario@hotmail.com>
2017-08-22 09:26:21 -0300

    Fixing incoherence at last line of the matrix

--- T1/fGaussSeidel.m
+++ T1/fGaussSeidel.m
@@ -9,11 +9,11 @@ function x = fGaussSeidel(n, sI, criterio)
         for i = 2 : n/2
             x(i) = (1 - lambda) * x(i) + lambda * ((1 - x(i - 1) - x(i + 1) - x(i +25))/4);
         end
-            
+
         for i = (n/2) + 1: n - 1
             x(i) = (1 - lambda) * x(i) + lambda * ((2 - x(i - 25) - x(i - 1) - x(i + 1))/5);
         end
-        x(n) = 3 - x(n -1);
+        x(n) = (1 - lambda) * x(n) + lambda * (3 - x(n -1));
         dif = max(abs(x - sI));
         sI = x;
     end
