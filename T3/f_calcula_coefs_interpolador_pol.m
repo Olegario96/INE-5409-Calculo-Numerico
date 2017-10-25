ec7905ed2d79dc87ad2ce91869183c018eb82091
Olegario96 <gustavo-olegario@hotmail.com>
2017-10-25 09:10:59 -0200

    Tchebyshev finished. I dare you type his name without looking for in Google

--- T3/f_calcula_coefs_interpolador_pol.m
+++ T3/f_calcula_coefs_interpolador_pol.m
@@ -4,8 +4,8 @@ function coefs = f_calcula_coefs_interpolador_pol(n, valores_x, valores_y)
     for j = 2 : n + 1
       A(i, j) = A(i, j - 1) * valores_x(i);
     end
-    y(i) = valores_y(i);
   end
 
-  coefs = fGauss([A transpose(y)]);
+  b = transpose(valores_y);
+  coefs = A \ b;
 end
--- T3/main.m
+++ T3/main.m
@@ -8,7 +8,7 @@ numero_pontos = 100;
 h = (lim_superior - lim_inferior) / n;
 valores_x = lim_inferior : h : lim_superior;
 
-y_exato = log(valores_x);
+y_exato = f_analisada(valores_x, lim_inferior, lim_superior);;
 
 %---- Interpolador polinomial ----%
 "Coeficientes do polinomio interpolador"
@@ -19,27 +19,36 @@ y_aprox_interpolador = f_pn_horner(n, coefs_interpolador, valores_x)
 
 "Erro maximo do interpolador polinomial"
 erro_interpolador_polinomial = max(abs(y_aprox_interpolador - y_exato))
-
 %---- Serie Maclauren ----%
 "Coeficientes calculados por MacLauren"
 n = 110;
 coefs_maclauren = f_calcula_ln_maclauren(n)
 
+"Valores de Y calculado por Horner aplicado nos coeficientes calculados"
 % Aqui existe necessidade de recalcular o h e valores_x pois a serie de
 % MacLauren utiliza numero de pontos.
 h = (lim_superior - lim_inferior) / numero_pontos;
 valores_x = lim_inferior : h : lim_superior;
 
 % Formula da TL nesse problema
-tp = (2/1.9) .* valores_x  .-(2.1/1.9);
-y_exato = log(valores_x);
-
-"Valores de Y calculado por Horner aplicado nos coeficientes calculados"
-y_aprox_maclauren = f_pn_horner(n, coefs_maclauren, tp)
+valores_t = (2/1.9) .* valores_x  .-(2.1/1.9);
+y_exato = f_analisada(valores_t, lim_inferior, lim_superior);
+y_aprox_maclauren = f_pn_horner(n, coefs_maclauren, valores_t)
 
 "Erro maximo de MacLauren"
 erro_maclauren = max(abs(y_aprox_maclauren - y_exato))
 
 %---- Tchebyshev ----%
+"Coeficientes calculados por Thcebyshev"
+n =30;
+coefs_tchebyshev = f_coef_tchebychev(n, lim_inferior, lim_superior)
+
+"Valores de  Y calculados por Horner aplicado nos coeficientes calculados"
+valores_x = -1 : (1 + 1)/n : 1;
+y_exato = f_analisada(valores_x, lim_inferior, lim_superior);
+y_aprox_tchebyshev = f_calcula_tchebychev(n, coefs_tchebyshev, valores_x)
+
+"Erro maximo de Thcebyshev"
+erro_tchebyshev = max(abs(y_aprox_tchebyshev - y_exato))
 
 %---- Padé ----%
