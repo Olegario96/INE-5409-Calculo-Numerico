e94e7008d8ea286eb38db48ab729e23b44e9395a
Olegario96 <gustavo-olegario@hotmail.com>
2017-10-28 12:29:31 -0200

    Pade finished

--- T3/main.m
+++ T3/main.m
@@ -14,20 +14,27 @@ valores_x_para_analisar = lim_inferior : h : lim_superior;
 y_exato = f_analisada(valores_x, lim_inferior, lim_superior);
 
 %---- Interpolador polinomial ----%
+"Grau do polinômio interpolador linear"
+n
+
 "Coeficientes do polinomio interpolador"
 coefs_interpolador = f_calcula_coefs_interpolador_pol(n, valores_x, y_exato)
 
 "Valores de Y calculado por Horner nos coeficientes calculados"
-y_exato = f_analisada(valores_x_para_analisar, lim_inferior, lim_superior)
-y_aprox_interpolador = f_pn_horner(n, coefs_interpolador, valores_x_para_analisar)
+y_exato = f_analisada(valores_x_para_analisar, lim_inferior, lim_superior);
+y_aprox_interpolador = f_pn_horner(n, coefs_interpolador, valores_x_para_analisar);
+transpose(y_aprox_interpolador)
 
 "Erro maximo do interpolador polinomial"
 erro_interpolador_polinomial = max(abs(y_aprox_interpolador - y_exato))
 
 %---- Serie Maclauren ----%
+"Grau do polinômio interpolador MacLauren"
+n = 110
+
 "Coeficientes calculados por MacLauren"
-n = 110;
-coefs_maclauren = f_calcula_ln_maclauren(n)
+coefs_maclauren = f_calcula_ln_maclauren(n);
+transpose(coefs_maclauren)
 
 "Valores de Y calculado por Horner aplicado nos coeficientes calculados"
 % Aqui existe necessidade de recalcular o h e valores_x pois a serie de
@@ -36,27 +43,49 @@ coefs_maclauren = f_calcula_ln_maclauren(n)
 % Formula da TL nesse problema
 valores_t = (2/1.9) .* valores_x_para_analisar  .-(2.1/1.9);
 y_exato = f_analisada(valores_t, lim_inferior, lim_superior);
-y_aprox_maclauren = f_pn_horner(n, coefs_maclauren, valores_t)
+y_aprox_maclauren = f_pn_horner(n, coefs_maclauren, valores_t);
+transpose(y_aprox_maclauren)
 
 "Erro maximo de MacLauren"
 erro_maclauren = max(abs(y_aprox_maclauren - y_exato))
 
 %---- Tchebyshev ----%
-"Coeficientes calculados por Thcebyshev"
-n =30;
-coefs_tchebyshev = f_coef_tchebychev(n, lim_inferior, lim_superior)
+"Grau do polinômio interpolador Tchebyshev"
+n =30
 
-"Valores de  Y calculados por Horner aplicado nos coeficientes calculados"
+"Coeficientes calculados por Thcebyshev"
+coefs_tchebyshev = f_coef_tchebychev(n, lim_inferior, lim_superior);
+transpose(coefs_tchebyshev)
+
+"Valores de  Y calculados por Horner aplicado nos coeficientyes calculados"
 valores_x = -1 : (1 + 1)/n : 1;
 y_exato = f_analisada(valores_x, lim_inferior, lim_superior);
 y_aprox_tchebyshev = f_calcula_tchebychev(n, coefs_tchebyshev, valores_x)
+transpose(y_aprox_tchebyshev)
 
-"Erro maximo de Thcebyshev"
+"Erro maximo de Tchebyshev"
 erro_tchebyshev = max(abs(y_aprox_tchebyshev - y_exato))
 
-%---- Padé ----%
-%npade = n
-%mpade = m
-%coefs_maclauren = f_calcula_ln_maclauren(npade + mpade)
-%[numerador_pade denominador_pade] = f_calcula_pade(npade, mpade, coefs_maclauren)
-%y_pade = f_calcula_maclauren(npade, numerador_pade, tp) ./ f_calcula_maclauren(mpade, denominador_pade, tp)
+%---- Pade ----%
+"Grau do polinômio interpolador de Pade do numerador"
+npade = 11
+
+"Grau do polinômio interpolador de Pade do denominador"
+mpade = 11
+
+coefs_maclauren = f_calcula_ln_maclauren(npade + mpade);
+
+[numerador_pade denominador_pade] = f_calcula_pade(npade, mpade, coefs_maclauren);
+
+"Coeficientes do numerador"
+transpose(numerador_pade)
+
+"Coeficientes do denominador"
+transpose(denominador_pade)
+
+"Valores de  Y calculados por Horner aplicado nos coeficientes calculados"
+y_pade = f_pn_horner(npade, numerador_pade, valores_x) ./ f_pn_horner(mpade, denominador_pade, valores_x);
+transpose(y_pade)
+
+"Erro maximo de Pade"
+erro_pade = max(abs(y_exato - y_pade))
