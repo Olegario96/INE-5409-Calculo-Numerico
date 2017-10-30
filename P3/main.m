3c00eaeb1b07f78ad53acb2bf2e2ad4cb2544bd6
Olegario96 <gustavo-olegario@hotmail.com>
2017-10-30 15:01:38 -0200

    Enhence the index for error

--- P3/main.m
+++ P3/main.m
@@ -24,17 +24,11 @@ y_exato = f_analisada(valores_x_para_analisar, lim_inferior, lim_superior);
 y_aprox_interpolador = f_pn_horner(n, coefs_interpolador, valores_x_para_analisar);
 
 "Erro maximo do interpolador polinomial"
-erro_interpolador_polinomial = max(abs(y_aprox_interpolador - y_exato))
+[erro_interpolador_polinomial erro_index] = max(abs(y_aprox_interpolador - y_exato))
 
 
 "Valor de X onde o erro eh maximo"
-for i = 1 : length(valores_x_para_analisar)
-  erro = f_pn_horner(n, coefs_interpolador, valores_x_para_analisar(i)) - f_analisada(valores_x_para_analisar(i), lim_inferior, lim_superior);
-  if abs(erro) == erro_interpolador_polinomial
-  	valor_x = valores_x_para_analisar(i);
-  endif
-end
-valor_x
+valores_x_para_analisar(erro_index)
 
 %---- Serie Maclauren ----%
 "Grau do polinômio interpolador MacLauren"
@@ -44,20 +38,13 @@ n = 11
 coefs_maclauren = f_calcula_integral_maclauren(n);
 transpose(coefs_maclauren)
 
-y_exato = f_analisada(valores_x_para_analisar, lim_inferior, lim_superior);
 y_aprox_maclauren = f_pn_horner(n, coefs_maclauren, valores_x_para_analisar);
 
 "Erro maximo de MacLauren"
-erro_maclauren = max(abs(y_aprox_maclauren - y_exato))
+[erro_maclauren erro_index] = max(abs(y_aprox_maclauren - y_exato))
 
 "Valor de X onde o erro eh maximo"
-for i = 1 : length(valores_x_para_analisar)
-  erro = f_pn_horner(n, coefs_maclauren, valores_x_para_analisar(i)) - f_analisada(valores_x_para_analisar(i), lim_inferior, lim_superior);
-  if abs(erro) == erro_maclauren
-  	valor_x = valores_x_para_analisar(i);
-  endif
-end
-valor_x
+valores_x_para_analisar(erro_index)
 
 %---- Tchebyshev ----%
 "Grau do polinômio interpolador Tchebyshev"
@@ -71,16 +58,10 @@ y_exato = f_analisada(valores_x, lim_inferior, lim_superior);
 y_aprox_tchebyshev = f_calcula_tchebychev(n, coefs_tchebyshev, valores_x);
 
 "Erro maximo de Tchebyshev"
-erro_tchebyshev = max(abs(y_aprox_tchebyshev - y_exato))
+[erro_tchebyshev erro_index] = max(abs(y_aprox_tchebyshev - y_exato))
 
 "Valor de X onde o erro eh maximo"
-for i = 1 : length(valores_x_para_analisar)
-  erro = f_pn_horner(n, coefs_tchebyshev, valores_x_para_analisar(i)) - f_analisada(valores_x_para_analisar(i), lim_inferior, lim_superior);
-  if abs(erro) == erro_tchebyshev
-  	valor_x = valores_x_para_analisar(i);
-  endif
-end
-valor_x
+valores_x_para_analisar(erro_index)
 
 %---- Pade ----%
 "Grau do polinômio interpolador de Pade do numerador"
@@ -102,13 +83,7 @@ transpose(denominador_pade)
 y_pade = f_pn_horner(npade, numerador_pade, valores_x) ./ f_pn_horner(mpade, denominador_pade, valores_x);
 
 "Erro maximo de Pade"
-erro_pade = max(abs(y_exato - y_pade))
+[erro_pade erro_index] = max(abs(y_exato - y_pade))
 
 "Valor de X onde o erro eh maximo"
-for i = 1 : length(valores_x_para_analisar)
-  erro = (f_pn_horner(npade, numerador_pade, valores_x_para_analisar(i)) / f_pn_horner(mpade, denominador_pade, valores_x_para_analisar(i))) - f_analisada(valores_x_para_analisar(i), lim_inferior, lim_superior);
-  if abs(erro) == erro_pade
-  	valor_x = valores_x_para_analisar(i);
-  endif
-end
-valor_x
+valores_x_para_analisar(erro_index)
